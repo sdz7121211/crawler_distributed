5e141bfdb70f0748b70d64e87564ec4603e2f891
sdz <670108918@qq.com>
2014-05-17 16:22:40 +0000

    change config

--- crawler/celeryconfig.py
+++ crawler/celeryconfig.py
@@ -3,8 +3,8 @@ import sys
 sys.path.insert(0, os.getcwd())
 print "celery config path:", os.getcwd()
 
-BROKER_URL = "redis://103.29.133.171:6379/11"
+BROKER_URL = "redis://103.29.133.*:6379/11"
 # CELERY_IMPORTS = ("downloader", "parse")
-CELERY_RESULT_BACKEND = "redis://103.29.133.171:6379/12"
+CELERY_RESULT_BACKEND = "redis://103.29.133.*:6379/12"
 CELERY_DISABLE_RATE_LIMITS = True
 CELERY_TASK_RESULT_EXPIRES = 1*60
